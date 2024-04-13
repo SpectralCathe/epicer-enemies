337a948cf21dc4a94ed90a62d06221f103a680d6
Cathe <spectral.cathe@gmail.com>
2024-04-13 18:28:29 +0300

    Lower defiance centurions cost

--- Scripts/CenturionDefiance.lua
+++ Scripts/CenturionDefiance.lua
@@ -7,7 +7,7 @@ local effects = {
         Name = "Dragon RAWR :3",
         ID = "CATHE_Archetype_CenturionDragon",
         Description = "Don’t stand in my safe space! :(",
-        Cost = 25,
+        Cost = 15,
         Weight = 1,
         Visible = true,
         Priority = 20,
@@ -26,7 +26,7 @@ local effects = {
         Name = "BlyadSuka",
         ID = "CATHE_Archetype_CenturionTendies",
         Description = "Blood for the flayed twins!",
-        Cost = 20,
+        Cost = 15,
         Weight = 1,
         Visible = true,
         Priority = 20,
@@ -34,8 +34,8 @@ local effects = {
             "Target_HeavyAttack"
         },
         SpecialLogic = {
-            "Ascension_Centurion_ACT_EndOfTurnDefiance",   -- Arena 3.1
-            "Ascension_Centurion_MUTA_SIRupturedTendons"   -- The Centurion 5.1
+            "Ascension_Centurion_ACT_EndOfTurnDefiance",     -- Arena 3.1
+            "Ascension_Centurion_MUTA_SIRupturedTendons",    -- The Centurion 5.1
         },
         Keyword = { Keyword = "Centurion", BoonType = "Activator" },
         KeywordStats = { "Defiance" }
