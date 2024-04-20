aa9bb37d432ea5d56474823e992c7536d9a40553
Cathe <spectral.cathe@gmail.com>
2024-04-20 12:22:17 +0300

    Add a paucity preset

---
+++ Mods/EpicerEnemies_071fcaaa-cf60-4982-9474-b797b8178509/Story/RawFiles/Lua/Scripts/Paucity.lua
@@ -0,0 +1,68 @@
+local EpicEnemies = Mods.EpipEncounters.Epip.GetFeature("Feature_EpicEnemies")
+
+---@type Features.EpicEnemies.Effect[]
+local effects = {
+    -- ACTIVATORS
+    {
+        Name = "Paucity 7 BH",
+        ID = "CATHE_Ascension_Paucity7BH",
+        Description = "Once per round, Paucity activates when you reach Battered or Harried 7 and above.",
+        Cost = 6,
+        Weight = 0,
+        Visible = false,
+        Priority = 20,
+        SpecialLogic = {
+            "Ascension_Paucity_ACT_BHStacks",  -- Hatchet 4.0
+        },
+        Keyword = { Keyword = "Paucity", BoonType = "Activator" },
+    },
+
+    -- MUTATORS
+    {
+        Name = "Paucity Wither on basic attacks Base",
+        ID = "CATHE_Ascension_PaucityWither_Base",
+        Description = "If you have Paucity Active, apply Withered for 1 turn to enemies hit while performing a basic attack.",
+        Cost = 0,
+        Weight = 0,
+        Visible = false,
+        Priority = 20,
+        Keyword = { Keyword = "Wither", BoonType = "Activator" },
+    },
+    {
+        Name = "Paucity Wither on basic attacks",
+        ID = "CATHE_Ascension_PaucityWither",
+        Description = "If you have Paucity Active, apply Withered for 1 turn to enemies hit while performing a basic attack.",
+        Cost = 10,
+        Weight = 0,
+        Visible = false,
+        Priority = 20,
+        Prerequisites = {
+            ["CATHE_Ascension_PaucityWither_Base"] = true,
+        },
+        SpecialLogic = {
+            "Ascension_Paucity_MUTA_WitherACT_BasicAttack",  -- Wrath 4.1
+        },
+        Keyword = { Keyword = "Paucity", BoonType = "Mutator" },
+    },
+
+    -- EFFECTS
+    {
+        Name = "Oppression-suppression",
+        ID = "CATHE_Effect_PaucityOnBHWither",
+        Description = FormatDescription("I suck power from thee!", "Paucity on 7 BH, Paucity makes basic attacks apply Wither."),
+        Cost = 16,
+        Weight = 1,
+        Visible = true,
+        Priority = 20,
+        Prerequisites = {
+            ["CATHE_Ascension_Paucity7BH"] = true,
+            ["CATHE_Ascension_PaucityWither_Base"] = true,
+            ["CATHE_Ascension_PaucityWither"] = true,
+        },
+        Keyword = { Keyword = "Predator", BoonType = "Mutator" },
+    },
+}
+
+for _,effect in ipairs(effects) do
+    EpicEnemies.RegisterEffect(effect.ID, effect)
+end
